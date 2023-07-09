db333e7f0f359f0a160d0703cf3a51a625fa6bad
Muhammad Zohaib <67507969+mzohaib07@users.noreply.github.com>
2023-07-09 15:25:16 +0500

    changes-pushged-to-git

---
+++ server/client/webpack.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.[fullhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // devServer: {
+  //   port: 3030 // you can change the port
+  // },
+  plugins: [
+    // 'plugin:react-hooks-unreliable-deps/recommended',
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import Routes from "./routes/routes.js";
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 
 import Connection from "./database/db.js";
 
@@ -13,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", Routes);
 
 const PORT = process.env.PORT || 3500;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+}
 
 Connection();
 app.listen(PORT, () => {
